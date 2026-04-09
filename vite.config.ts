1982d67039bac9269e15be52ffeef0661d3e3279
Alexandre Rodenas <12912920+alexandrerodenas@users.noreply.github.com>
2026-04-09 09:43:56 +0200

    WebGPU fixing build

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({mode}) => {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
+        '@mlc-ai/web-llm': path.resolve(__dirname, 'node_modules/@mlc-ai/web-llm/lib/index.js'),
       },
     },
     server: {
